677700aac6c919a658234351ee2f422f14aad1db
"gheorghemolnar" <"molnargheorghe@yahoo.com">
2022-06-09 15:06:35 +0200

    update TP Voting: include majority detection

--- TP-Voting.sol
+++ TP-Voting.sol
@@ -10,10 +10,12 @@ struct Voter {
     bool hasVoted;
     uint votedProposalId;
 }
+
 struct Proposal {
     string description;
     uint voteCount;
 }
+
 enum WorkflowStatus {
     RegisteringVoters,
     ProposalsRegistrationStarted,
@@ -24,12 +26,14 @@ enum WorkflowStatus {
 }
 
 contract Voting is Ownable{
-    uint nonce = 0;
-    mapping (address => Voter)  voterslist;
-    mapping (uint => Proposal)  proposalslist;
-    WorkflowStatus       public votingStatus = WorkflowStatus.RegisteringVoters;
-    uint                 public winningProposalId;
+    uint                        nonce = 0;                                          // used to register the Proposol number
+    WorkflowStatus       public votingStatus = WorkflowStatus.RegisteringVoters;    // Workflow Status
+    uint                 public winningProposalId;                                  // WinnerId (computed after Vote closing)
 
+    mapping (address => Voter)  voterslist;         // Registered voters list
+    mapping (uint => Proposal)  proposalslist;      // Registered proposals list
+
+    // Events emited
     event VoterRegistered(address voterAddress);
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
@@ -38,6 +42,7 @@ contract Voting is Ownable{
     function voterRegistration(address _voter) public onlyOwner () {
         require(votingStatus == WorkflowStatus.RegisteringVoters, "Voter registration no longer available !");
         require(!voterslist[_voter].isRegistered, "Voter already registrerd !");
+
         Voter memory newVoter = Voter ({isRegistered: true, hasVoted: false, votedProposalId: 0 });
         voterslist[_voter] = newVoter;
         emit VoterRegistered(_voter);
@@ -46,6 +51,8 @@ contract Voting is Ownable{
     function proposalRegistration(string memory _description) public{
         require(votingStatus == WorkflowStatus.ProposalsRegistrationStarted, "Proposal registration is not possible now.");
         require(voterslist[msg.sender].isRegistered, "Only registred voters can submit a proposal.");
+
+        // used to determine if there are duplicate of a proposal
         bool duplicateProposition = indexOf(_description) != -1;
         require(!duplicateProposition, "Proposition already registered !");
 
@@ -83,6 +90,7 @@ contract Voting is Ownable{
 
         emit WorkflowStatusChange(prevVotingStatus, votingStatus);
 
+        // calculate the winner
         winningProposalId = getWinner();
     }
 
@@ -93,22 +101,41 @@ contract Voting is Ownable{
 
         voterslist[msg.sender].hasVoted = true;
         voterslist[msg.sender].votedProposalId = _proposalId;
-        proposalslist[_proposalId].voteCount += 1;
+
+        // _proposalId = 0 => Empty vote, no voteCount to update
+        if(_proposalId > 0){
+            proposalslist[_proposalId].voteCount += 1;
+        }
 
         emit Voted(msg.sender, _proposalId);
     }
 
+    // compute the winner
     function getWinner() private view returns(uint) {
         require(votingStatus == WorkflowStatus.VotingSessionEnded, "Voting process has not ended !");
-        uint winner = 0;
-        for (uint i=0; i<=nonce; i++){
+
+        uint totalVoters    = 0;
+        uint winner         = 0;
+
+        for (uint i=1; i<=nonce; i++){
+            totalVoters +=1;
             if(proposalslist[i].voteCount > winner) {
                 winner = i;
             }
         }
+        if(winner == 0){
+            return 0;
+        }
+
+        uint majority       = (totalVoters / 2) + 1;
+        if(winner < majority){
+            return 0;
+        }
+
         return winner;
     }
 
+    // used to check for Proposal duplicates
     function indexOf(string memory value) private view returns(int){
         for (uint i = 1; i <= nonce; i++) {
             if (keccak256(abi.encodePacked(proposalslist[i].description)) == keccak256(abi.encodePacked(value))) {
